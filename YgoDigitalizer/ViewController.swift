3b4ece421ed2d83d0387ff66e20f61301a3c08c3
Bruno Thuma <bruno.thuma@hotmail.com>
2021-09-30 10:08:40 -0300

    Initial Commit

---
+++ YgoDigitalizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YgoDigitalizer
+//
+//  Created by Bruno Thuma on 30/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
